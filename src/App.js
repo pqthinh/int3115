d659cb0b3cda36f2fcdc2fdc9c3e140b88523e2a
thinhpq <thinhpq@its-global.vn>
2021-03-11 23:11:08 +0700

    config app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import * as Linking from "expo-linking";
+import * as SplashScreen from "expo-splash-screen";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import moment from "moment";
+// import messaging from "@react-native-firebase/messaging";
 
-export default function App() {
+import { persistor, store } from "./stores/configureStore";
+import AppNavigator from "./navigation/index";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+import LoadingProvider from "./providers/loadingProvider";
+
+import "moment/locale/ja";
+moment.locale("ja");
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+};
+
+const App = () => {
+  const [state, setState] = React.useState({
+    appIsReady: false,
+  });
+  const [fontLoader, setFontLoader] = React.useState(true);
+
+  React.useEffect(() => {
+    async () => {
+      try {
+        prepareResources();
+        await SplashScreen.preventAutoHideAsync();
+
+        // const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+        //   console.log("FCM Message Data:", remoteMessage.data);
+          // Update a users messages list using AsyncStorage
+          // const currentMessages = await AsyncStorage.getItem('messages');
+          // const messageArray = JSON.parse(currentMessages);
+          // messageArray.push(remoteMessage.data);
+          // await AsyncStorage.setItem('messages', JSON.stringify(messageArray));
+        // });
+
+        return unsubscribe;
+      } catch (e) {
+        console.warn(e);
+      }
+    };
+  }, []);
+
+  const useMount = (func) => React.useEffect(() => func(), []);
+
+  const useInitialURL = () => {
+    const [url, setUrl] = React.useState(null);
+    const [processing, setProcessing] = React.useState(true);
+
+    useMount(() => {
+      const getUrlAsync = async () => {
+        // Get the deep link used to open the app
+        const initialUrl = await Linking.parseInitialURLAsync();
+
+        // The setTimeout is just for testing purpose
+        setTimeout(() => {
+          setUrl(initialUrl);
+          setProcessing(false);
+        }, 1000);
+      };
+
+      getUrlAsync();
+    });
+
+    return { url, processing };
+  };
+
+  const { url: initialUrl, processing } = useInitialURL();
+
+  const prepareResources = async () => {
+    // await performAPICalls();
+    // await downloadAssets();
+
+    setState({ appIsReady: true }, async () => {
+      await SplashScreen.hideAsync();
+    });
+  };
+
+  if (fontLoader && !state.appIsReady) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoader(false)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Text>pqthinh custom</Text>
-      <Text>Hello world</Text>
-      <Text>test</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <LoadingProvider>
+          <AppNavigator />
+        </LoadingProvider>
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
